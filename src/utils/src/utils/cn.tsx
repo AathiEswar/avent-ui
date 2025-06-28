6378ea7c2a4efc007b8ddad9ffda314de9dad217
Aathi Eswar <aathi28082003@gmail.com>
2025-06-28 11:28:55 +0530

    testing done

--- src/utils/src/utils/cn.tsx
+++ src/utils/src/utils/cn.tsx
@@ -1,7 +1,7 @@
 import { clsx, ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge"
 
-function cn(classValue: ClassValue[]) {
+function cn(...classValue: ClassValue[]) {
   return (twMerge(clsx(...classValue)))
 }
 
